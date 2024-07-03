f4f90c4b004b1cd25c0b568e6fc14a73a76449bd
tshenkai <70874898+tianxin-shen@users.noreply.github.com>
2024-07-03 14:34:41 -0400

    adjust size of card

--- ytspace/src/app/components/molecules/codeCard.tsx
+++ ytspace/src/app/components/molecules/codeCard.tsx
@@ -7,7 +7,7 @@ import { Repo } from "@/app/assets/utils";
 export function CodeCard({ repos }: { repos: Repo[] }) {
   return (
     <div
-      className="flex flex-col w-100 md:w-[25rem] md:p-[0.625rem] md:rounded-xl rounded-lg bg-zinc-900"
+      className="flex flex-col w-100 md:max-w-[30rem] md:p-[0.625rem] md:rounded-xl rounded-lg bg-zinc-900"
       style={{ border: "1px solid " + BORDER }}
     >
       <Tag tag="Code Library" />
