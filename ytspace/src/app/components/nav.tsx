3c24a9e502348ec6daeb727f96c2a2dd70363225
tshenkai <70874898+tianxin-shen@users.noreply.github.com>
2024-07-03 14:32:11 -0400

    add nav

--- ytspace/src/app/components/nav.tsx
+++ ytspace/src/app/components/nav.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import TextButton from "./atoms/textButton";
 
 const navItems = {
   "/": {
@@ -7,23 +8,29 @@ const navItems = {
   "/myjourney": {
     name: "journey",
   },
-  "https://github.com/tianxin-shen": {
+  "https://github.com/yetongumich": {
     name: "github",
+  },
+  "https://scholar.google.com/citations?user=Js_AA5IAAAAJ&hl=en": {
+    name: "Gscholar",
   },
 };
 
 export function Navbar() {
   return (
     <aside>
-      <div className="lg:sticky lg:top-20">
+      <div
+        className="lg:sticky lg:top-20"
+        style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+      >
         <nav id="nav">
-          <div className="flex flex-row space-x-10 py-5 bg-neutral-900 flex justify-center">
+          <div className="flex flex-row md:gap-[3.125rem] gap-[0.625rem] md:p-[1.25rem] p-[1rem] bg-zinc-950 justify-center">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="font-mono hover:text-teal-400 py-1 px-2 m-1"
+                  className="font-mono hover:text-teal-400 text-[14px] md:text-[20px] p-[0.3125rem]"
                 >
                   {name}
                 </Link>
